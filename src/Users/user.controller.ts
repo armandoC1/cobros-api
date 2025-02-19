6e42ba19e5ea64f13b59cd51b4e9409e2610475f
armandoC1 <hr24006@clases.edu.sv>
2025-02-19 06:40:16 -0600

    add findByUsername on user conroller

--- src/Users/user.controller.ts
+++ src/Users/user.controller.ts
@@ -1,33 +1,47 @@
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
-import { UserService } from "./user.service";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { RolesGuard } from "src/auth/roles.guard";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly usersService: UserService) { }
+  constructor(private readonly usersService: UserService) {}
 
-    @Post()
-    @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
-    async createUser(@Body() body: { fullName: string }) {
-        return this.usersService.createUser(body.fullName);
-    }
+  @Post()
+  @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
+  async createUser(@Body() body: { fullName: string }) {
+    return this.usersService.createUser(body.fullName);
+  }
 
-    @Get()
-    @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
-    async findAll() {
-        return this.usersService.findAllUsers();
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
+  async findByUsername(@Param('username') username: string) {
+    return this.usersService.findByUsername(username);
+  }
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    async findOne(@Param('id') id: number) {
-        return this.usersService.findUserById(id);
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
+  async findAll() {
+    return this.usersService.findAllUsers();
+  }
 
-    @Patch(':id')
-    @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
-    async removeUser(@Param('id') id: number) {
-        return this.usersService.remove(id);
-    }
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async findOne(@Param('id') id: number) {
+    return this.usersService.findUserById(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, new RolesGuard(['admin']))
+  async removeUser(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
 }
